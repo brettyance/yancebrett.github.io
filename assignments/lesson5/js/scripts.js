ef255d8d6dc6336fd42000b3cf9aab0e2f7828e5
bayace92 <bayace92@bayace92-02.masteryguy.com>
2019-10-19 23:25:16 -0500

    Ready!

--- assignments/lesson5/js/scripts.js
+++ assignments/lesson5/js/scripts.js
@@ -1,41 +1,60 @@
+var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+var dateC = new Date();
+
+var day = days[dateC.getDay()];
+var month = months[dateC.getMonth()];
+
+var date = `${day}, ${dateC.getDate()} ${month} ${dateC.getFullYear()}`;
+
+function setFriday() {
+
+    if (day=="Friday") {
+        document.getElementsByClassName("banner")[0].classList.toggle("friday");
+    }
+}
+
+function setDayOfWeek() {
+    for (i = 0; i < 5; i++) {
+        document.getElementById(`day${i + 1}name`).innerHTML = days[(dateC.getDay() + 1 + i) % 7];
+    }
+}
+
 function setDate() {
-
-    var days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-
-    var months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ];
-
-    var dateC = new Date();
-
-    var day = days[dateC.getDay()];
-    var month = months[dateC.getMonth()];
-
-    var date = `${day}, ${dateC.getDate()} ${month} ${dateC.getFullYear()}`;
 
     document.getElementById("date").innerHTML = date;
 }
 
-window.onload=setDate();
+function start() {
+    setFriday();
+    setDayOfWeek();
+    setDate();
+}
+
+window.onload=start();
 
 function openMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
