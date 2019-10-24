9625e974cf1bb34d7ae85ba8620a2a56000156d4
brettyancebyui <yan18009@byui.edu>
2019-10-23 23:49:09 -0500

    Created Lesson6 folder
    Images are already responsive. Still need to update Java and make sure
    everything is within frame.

---
+++ assignments/lesson6/js/scripts.js
@@ -0,0 +1,61 @@
+var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+var dateC = new Date();
+
+var day = days[dateC.getDay()];
+var month = months[dateC.getMonth()];
+
+var date = `${day}, ${dateC.getDate()} ${month} ${dateC.getFullYear()}`;
+
+function setFriday() {
+
+    if (day=="Friday") {
+        document.getElementsByClassName("banner")[0].classList.toggle("friday");
+    }
+}
+
+function setDayOfWeek() {
+    for (i = 0; i < 5; i++) {
+        document.getElementById(`day${i + 1}name`).innerHTML = days[(dateC.getDay() + 1 + i) % 7];
+    }
+}
+
+function setDate() {
+
+    document.getElementById("date").innerHTML = date;
+}
+
+function start() {
+    setFriday();
+    setDayOfWeek();
+    setDate();
+}
+
+window.onload=start();
+
+function openMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
